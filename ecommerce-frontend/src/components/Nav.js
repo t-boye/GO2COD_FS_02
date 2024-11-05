249753066f1bace40c038829c2b6effbda2595fc
Emmanuel Tete Boye <emmanuelboye1957@gmail.com>
2024-11-05 02:23:59 +0000

    commit

--- ecommerce-frontend/src/components/Nav.js
+++ ecommerce-frontend/src/components/Nav.js
@@ -2,7 +2,8 @@
 import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FaHome, FaShoppingCart, FaBars, FaTimes } from 'react-icons/fa';
-import { ShoppingCartContext } from '../context/ShoppingCartContext'; // Ensure this path is correct
+import { ShoppingCartContext } from '../context/ShoppingCartContext';
+import logo from '../assets/images/Online_Shop_Logo.png'; 
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,13 +16,19 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-900 p-4 shadow-md fixed w-full z-10">
       <div className="container mx-auto flex justify-between items-center px-8 md:px-16">
-        <h1 className="text-white text-2xl font-bold">Online Store</h1>
+        {/* Logo Section */}
+        <Link to="/" className="flex items-center">
+        <img src={logo}  alt="Logo" className="h-12" /> 
+        <h1 className="text-white text-2xl font-bold ml-2">Online Store</h1>
+        </Link>
+        
         <div className="hidden md:flex space-x-6">
           <NavLink to="/" icon={<FaHome />} label="Home" />
           <NavLink to="/products" label="Products" />
           <NavLink to="/about" label="About Us" />
           <CartLink count={getCartCount()} />
         </div>
+        
         <div className="md:hidden">
           <button onClick={toggleMenu} className="text-white" aria-label="Toggle menu">
             {isOpen ? <FaTimes /> : <FaBars />}
@@ -51,8 +58,8 @@ const NavLink = ({ to, icon, label }) => (
 // CartLink component to display cart with count
 const CartLink = ({ count }) => (
   <Link to="/cart" className="flex items-center text-white hover:text-yellow-400 transition duration-300">
-    <FaShoppingCart className="mr-1" />
     Cart
+    <FaShoppingCart className="mr-1" />
     <span className="ml-1 bg-yellow-400 text-gray-900 rounded-full px-2 text-xs">
       {count}
     </span>
